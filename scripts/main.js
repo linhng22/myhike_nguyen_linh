08be153bf6a10213a9ad204401f05dccfcd82436
Linh Nguyen <knguyen142@my.bcit.ca>
2022-03-02 18:38:36 -0800

    wrote and read data in Firestore, added Bootstrap grid in the main page

--- scripts/main.js
+++ scripts/main.js
@@ -27,3 +27,66 @@ function insertName() {
     })
 }
 insertName();
+
+function writeHikes() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("hikes");
+
+    hikesRef.add({
+        code:"IC01",
+        name: "Inca Trail",   
+        country: "Peru",
+        level: "hard",
+        length: "42 km",
+        details: "The trail follows the route the ancient Incas took over 650 years ago, and much of the original stonework is still in place."
+    });
+    hikesRef.add({
+        code:"SG01",
+        name: "Samaria Gorge Trail",   
+        country: "Greece",
+        level: "moderate",
+        length: "16 km",
+        details: "Starting in pine forests, this one-way hike descends through a narrow valley, eventually ending at the Libyan Sea."
+    });
+    hikesRef.add({
+        code:"WC01",
+        name: "West Coast Trail",  
+        country: "Canada",
+        level: "very hard",
+        length: "72 km",
+        details: "Located along Vancouver Island's wild Pacific Coast, the West Coast Trail is one of the most spectacular maritime hikes in Canada."
+    });
+}
+
+// writeHikes(); 
+
+
+function displayCards(collection) {
+    let cardTemplate = document.getElementById("hikeCardTemplate");
+
+    db.collection(collection).get()
+        .then(snap => {
+            var i = 1;
+            snap.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;   // get value of the "name" key
+                var details = doc.data().details;   // get value of the "details" key
+                let newcard = cardTemplate.content.cloneNode(true);
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = "./images/hike" + i + ".jpg"; //hikes.jpg
+
+                //give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+                i++;
+            })
+        })
+}
+
+displayCards("hikes");
